99a12e4f3946e93bda880f1cf111fecd7784fccd
XtraCube <72575280+XtraCube@users.noreply.github.com>
2026-06-17 14:08:14 -0400

    clear warnings

--- Generator/ResponseModels/ChangesetResponse.cs
+++ Generator/ResponseModels/ChangesetResponse.cs
@@ -4,9 +4,7 @@ namespace Generator.ResponseModels;
 
 public class ChangesetResponse
 {
-    [JsonPropertyName("version")]
-    public string Version { get; set; }
+    [JsonPropertyName("version")] public string Version { get; set; } = null!;
 
-    [JsonPropertyName("changeset")]
-    public string Changeset  { get; set; }
+    [JsonPropertyName("changeset")] public string Changeset { get; set; } = null!;
 }
